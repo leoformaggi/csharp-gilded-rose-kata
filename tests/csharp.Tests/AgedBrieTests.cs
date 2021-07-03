c1567dd13997a9a6e8d1549a6380f1668a30038f
leonardo.formaggi <leo.formaggi@gmail.com>
2021-07-03 20:24:04 -0300

    Removing the added specialized tests because the custom item classes changed and they're not needed anymore, since they're being tested through GildedRose class. After refactoring, the old tests still pass normally, which indicates that the refactoring worked properly.

--- tests/csharp.Tests/AgedBrieTests.cs
+++ tests/csharp.Tests/AgedBrieTests.cs
@@ -25,29 +25,17 @@ namespace csharp.Tests
         }
 
         [Test]
-        public void Aged_brie_quality_should_raise_everyday_specialized()
-        {
-            var agedBrie = new AgedBrie(10, 0);
-
-            Assert.AreEqual(0, agedBrie.Quality);
-            for (int i = 1; i <= 10; i++)
-            {
-                agedBrie.AdvanceDay();
-
-                if (agedBrie.SellIn > 0)
-                    Assert.AreEqual(i, agedBrie.Quality);
-            }
-        }
-
-        [Test]
         public void Aged_brie_quality_should_not_be_higher_than_50_specialized()
         {
-            var item = new AgedBrie(10, 49);
+            Item agedBrie = new Item { Name = "Aged Brie", SellIn = 10, Quality = 49 };
+            var items = new List<Item> { agedBrie };
+
+            var app = new GildedRose(items);
 
             for (int i = 0; i < 3; i++)
-                item.AdvanceDay();
+                app.UpdateQuality();
 
-            Assert.AreEqual(50, item.Quality);
+            Assert.AreEqual(50, agedBrie.Quality);
         }
     }
 
--- tests/csharp.Tests/GenericItemsTests.cs
+++ tests/csharp.Tests/GenericItemsTests.cs
@@ -60,52 +60,5 @@ namespace csharp.Tests
                 day++;
             }
         }
-
-        [Test]
-        public void Generic_items_quality_should_not_be_negative_specialized()
-        {
-            var item = new GenericItem("foo", 0, 2);
-
-            for (int i = 0; i < 4; i++)
-                item.AdvanceDay();
-
-            Assert.AreEqual(0, item.Quality);
-        }
-
-        [Test]
-        public void Generic_items_quality_should_decrease_by_1_everyday_when_SellIn_is_positive_specialized()
-        {
-            int sellIn = 10;
-            int quality = 10;
-
-            var genericItem = new GenericItem("foo", sellIn, quality);
-
-            int day = 1;
-            while (genericItem.SellIn > 0)
-            {
-                genericItem.AdvanceDay();
-
-                Assert.AreEqual(quality - day, genericItem.Quality);
-                day++;
-            }
-        }
-
-        [Test]
-        public void Generic_items_quality_should_decrease_by_2_everyday_when_SellIn_is_negative_specialized()
-        {
-            int sellIn = 0;
-            int quality = 9;
-
-            var genericItem = new GenericItem("foo", sellIn, quality);
-
-            int day = 1;
-            while (genericItem.Quality > 1)
-            {
-                genericItem.AdvanceDay();
-
-                Assert.AreEqual(quality - (2 * day), genericItem.Quality);
-                day++;
-            }
-        }
     }
 }
